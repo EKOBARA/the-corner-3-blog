cf98e63befeb2a707875d7c5566fdae4153f4c89
ekobara <emmanuelkobara@gmail.com>
2021-10-13 22:43:36 -0400

    Added Nav component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './components/home';
+import Home from './components/Home';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <h1>
-       
+       Hello World
       </h1>
+      <Nav />
       <Home/>
     </div>
   );
