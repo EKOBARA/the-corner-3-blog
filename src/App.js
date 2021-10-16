779ecf1b7d4700ef0bf4f6d94c8947d7a81affb9
ekobara <emmanuelkobara@gmail.com>
2021-10-15 21:53:01 -0400

    Added Route and Link to different pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Home from './components/Home';
 import Nav from './components/Nav';
+import { Route } from 'react-router-dom';
+import Article from './components/Article';
+import Account from './components/Account';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-       Hello World
-      </h1>
       <Nav />
-      <Home/>
+      <Route exact path='/' component={Home} /> 
+      <Route exact path='/article' component={Article} /> 
+      <Route exact path='/account' component={Account} /> 
+      
     </div>
   );
 }
